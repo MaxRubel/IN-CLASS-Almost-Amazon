6aaf6a9b4ba5c6eebbbecd6a4d8f563ca4c40572
MaxRubel <maxrubel1@gmail.com>
2023-11-18 09:38:55 -0600

    fixed a small issue with main.js

--- public/main.js
+++ public/main.js
@@ -1,10 +1,8 @@
 import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
-
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
 const init = () => {
   ViewDirectorBasedOnUserAuthStatus();
 };
-
 init();
